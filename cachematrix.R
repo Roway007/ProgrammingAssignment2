538ed6a6864480f0169dd80b63e61ca544aa3a05
ROY QUIÑONEZ <57645736+Roway007@users.noreply.github.com>
2021-06-23 14:58:09 -0500

    Update cachematrix.R
    description part 2 y fin definitivo

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-##  La primera función, makeCacheMatrix crea una "matriz" especial, que es realmente una lista que contiene una función para
+##  La primera función, makeCacheMatrix crea una "matriz" especial, que es realmente una lista que contiene una función para:
 ## 1. establecer el valor de la matriz
 ## 2. obtener el valor de la matriz
 ## 3. establecer el valor de la inversa de la matriz
@@ -23,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## 
+## La función cacheSolve calcula la inversa de la "matriz" especial que se creó con la función makeCacheMatrix.
+## Primero verifica si la inversa ya se ha calculado y si es así, obtiene la inversa de la caché y omite el cálculo.
+## De lo contrario, calcula la inversa de la matriz y establece el valor de la inversa en el caché a través de la función setInverse.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'

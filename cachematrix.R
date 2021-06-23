f16ce17662e288a76b85cf50570481c211917ec7
ROY QUIÑONEZ <57645736+Roway007@users.noreply.github.com>
2021-06-23 14:45:04 -0500

    Update cachematrix.R
    description

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## 
+##  La primera función, makeCacheMatrix crea una "matriz" especial, que es realmente una lista que contiene una función para
+## 1. establecer el valor de la matriz
+## 2. obtener el valor de la matriz
+## 3. establecer el valor de la inversa de la matriz
+## 4. obtener el valor de la inversa de la matriz
 
 makeCacheMatrix <- function(x = matrix()) {
         j <- NULL
